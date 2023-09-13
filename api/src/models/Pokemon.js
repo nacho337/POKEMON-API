9796fa4d400f1eef31a5f58d513e4e9f67da333a
nacho337 <ignaciorossatti9@gmail.com>
2023-09-13 14:18:42 -0300

    database

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    idPoke: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    vida: {
+      type: DataTypes.INTEGER
+    },
+    fuerza: {
+      type: DataTypes.INTEGER
+    },
+    defensa: {
+      type: DataTypes.INTEGER
+    },
+    velocidad: {
+      type: DataTypes.INTEGER
+    },
+    altura: {
+      type: DataTypes.DECIMAL
+    },
+    peso: {
+      type: DataTypes.DECIMAL
+    }
   });
 };
---
+++ api/src/models/Tipo.js
@@ -0,0 +1,15 @@
+const { DataTypes, NUMBER } = require('sequelize');
+
+module.exports = sequelize => {
+    sequelize.define('tipo', {
+        slot: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            unique: true,
+        }
+    });
+}
