8a244849c3f4539781de11286367121053116a62
nacho337 <ignaciorossatti9@gmail.com>
2023-09-19 10:44:02 -0300

    db y back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const pokemonsRoute = require('./pokemons.js');
+const typesRoute = require('./types.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+router.use('/pokemons', pokemonsRoute); //api pokemons
+router.use('/types', typesRoute); //api types
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
