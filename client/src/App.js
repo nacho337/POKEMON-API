2557d56285c16f8ef5f5b86f6fb7cf9b0c28405e
nacho337 <ignaciorossatti9@gmail.com>
2023-09-26 11:56:43 -0300

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-import {Route, BrowserRouter, Switch} from "react-router-dom";
-//import "./App.css";
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+
 import Home from "./views/home/home.component";
 import Detail from "./views/detail/detail.component";
 import Create from "./views/create/create.component";
-
+//import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Switch>
-           <Route exact path="/home" component={Home}/>
-           <Route path="/home/:id" component={Detail}/>
-           <Route path="/create" component={Create}/>
-        </Switch>
-      </div>
+      <Routes>  
+           <Route exact path="/home" component={ <Home />}/>
+           <Route path="/home/:id" component={ <Detail />}/>
+           <Route path="/create" component={ <Create />}/>
+      </Routes>
     </BrowserRouter>
-    );
+  );
 }
 
-export default App ;
+export default App;
